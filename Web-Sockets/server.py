6455274413b940b323caf5ebffdf0e1e6b0a7d0c
[Jaypa92] <j.parris1@aol.com>
2024-12-04 20:04:34 -0500

    commit

--- Web-Sockets/server.py
+++ Web-Sockets/server.py
@@ -1,19 +1,39 @@
 import asyncio
 import websockets
 
-async def echo(websocket, path):
-    print(f"New connection from {websocket.remote_address}")
+rooms={}
+
+async def chat_handler(websocket, path):
     try:
+        room_name = await websocket.recv()
+        if room_name not in rooms:
+            rooms[room_name] = set()
+        rooms[room_name].add(websocket)
+
+        await asyncio.gather(
+            *(client.send(f"A new user joined {room_name}") 
+            for client in rooms[room_name] if client != websocket)
+        )
+
         async for message in websocket:
-            print(f"Received message: {message}")
-            await websocket.send(f"Echo: {message}")
-    except websockets.exceptions.ConnectionClosed as e:
-        print(f"Connection closed with error: {e}")
+            await asyncio.gather(
+                *(client.send(f"[{room_name}] {message}")
+                for client in rooms[room_name] if client != websocket
+                )
+            )
+    
+    except websockets.exceptions.ConnectionClosed:
+        print(f"Client disconnected from room : {room_name}")
+    finally:
+
+        rooms[room_name].remove(websocket)
+        if not rooms[room_name]:
+            del rooms[room_name]
 
 async def main():
-    server = await websockets.serve(echo, "localhost", 8765)
-    print("WebSocket server rinning on ws://localhost:8765")
+    server = await websockets.serve(chat_handler, "localhost", 8765)
+    print("Websocket server running on ws://localhost:8765")
     await server.wait_closed()
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(main()) 
